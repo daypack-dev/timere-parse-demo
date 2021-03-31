e869df8c23539b632bf9d95b67f118319e35c631
Darren Ldl <darrenldldev@gmail.com>
2021-03-31 22:49:05 +1100

    Formatting

--- src/demo.ml
+++ src/demo.ml
@@ -10,43 +10,45 @@ let () =
   let write_error s = write_msg ("Error: " ^ s) in
   input_box##.onkeydown :=
     Dom_html.handler (fun e ->
-        ( if e##.keyCode = 13 then
-            match
-              Timere_parse.timere
-              (Js.to_string input_box##.value)
-            with
-            | Error msg -> write_error msg
-            | Ok t -> (
-                let tz = Timere.Time_zone.utc in
-                match
-                  Timere.(resolve ~search_using_tz:tz (t & after (Date_time.now ~tz_of_date_time:tz ()) & before (Date_time.make_exn ~tz ~year:2050 ~month:`Jan ~day:1 ~hour:0 ~minute:0 ~second:0)))
-                with
-                | Error msg -> write_error msg
-                | Ok s -> (
-                    let l =
-                      s
-                      |> OSeq.take 100
-                      |> Seq.map (fun (x, y) ->
-                          let x =
-                            x
-                            |> Timere.Date_time.of_timestamp ~tz_of_date_time:tz
-                            |> Option.get
-                            |> Timere.Date_time.to_string
-                          in
-                          let y =
-                            y
-                            |> Timere.Date_time.of_timestamp ~tz_of_date_time:tz
-                            |> Option.get
-                            |> Timere.Date_time.to_string
-                          in
-                          Printf.sprintf "[%s, %s)" x y
-                        )
-                      |> List.of_seq
-                    in
-                    match l with
-                    | [] -> write_msg "No time slots found"
-                    | _ ->
-                      let str = String.concat "<br>" l in
-                      write_msg str ) ) );
+        (if e##.keyCode = 13 then
+           match Timere_parse.timere (Js.to_string input_box##.value) with
+           | Error msg -> write_error msg
+           | Ok t -> (
+               let tz = Timere.Time_zone.utc in
+               match
+                 Timere.(
+                   resolve ~search_using_tz:tz
+                     (t
+                      & after (Date_time.now ~tz_of_date_time:tz ())
+                      & before
+                        (Date_time.make_exn ~tz ~year:2050 ~month:`Jan ~day:1
+                           ~hour:0 ~minute:0 ~second:0)))
+               with
+               | Error msg -> write_error msg
+               | Ok s -> (
+                   let l =
+                     s
+                     |> OSeq.take 100
+                     |> Seq.map (fun (x, y) ->
+                         let x =
+                           x
+                           |> Timere.Date_time.of_timestamp ~tz_of_date_time:tz
+                           |> Option.get
+                           |> Timere.Date_time.to_string
+                         in
+                         let y =
+                           y
+                           |> Timere.Date_time.of_timestamp ~tz_of_date_time:tz
+                           |> Option.get
+                           |> Timere.Date_time.to_string
+                         in
+                         Printf.sprintf "[%s, %s)" x y)
+                     |> List.of_seq
+                   in
+                   match l with
+                   | [] -> write_msg "No time slots found"
+                   | _ ->
+                     let str = String.concat "<br>" l in
+                     write_msg str)));
         Js._true);
   ()
