2a489332a405ad53cfde0aeb09283fb3018d66b4
Darren Ldl <darrenldldev@gmail.com>
2021-03-31 19:41:13 +1100

    Updated to timere

--- src/demo.ml
+++ src/demo.ml
@@ -5,12 +5,6 @@ let () =
   let output_box_id = "outputBox" in
   let input_box = Js.Unsafe.global##.document##getElementById input_box_id in
   let output_box = Js.Unsafe.global##.document##getElementById output_box_id in
-  let search_param =
-    Daypack_lib.Search_param.make_using_years_ahead
-      ~start:(`Unix_second (Daypack_lib.Time.Current.cur_unix_second ()))
-      5
-    |> Result.get_ok
-  in
   output_box##.innerHTML := Js.string "output";
   let write_msg s = output_box##.innerHTML := Js.string s in
   let write_error s = write_msg ("Error: " ^ s) in
@@ -18,12 +12,14 @@ let () =
     Dom_html.handler (fun e ->
         ( if e##.keyCode = 13 then
             match
-              Daypack_lib.Time_expr.of_string (Js.to_string input_box##.value)
+              Timere_parse.timere
+              (Js.to_string input_box##.value)
             with
             | Error msg -> write_error msg
             | Ok expr -> (
                 match
-                  Daypack_lib.Time_expr.matching_time_slots search_param expr
+                  Timere.resolve
+                   expr
                 with
                 | Error msg -> write_error msg
                 | Ok s -> (
@@ -32,16 +28,16 @@ let () =
                       |> OSeq.take 100
                       |> Seq.map (fun (x, y) ->
                           let x =
-                            Daypack_lib.Time.To_string
-                            .yyyymondd_hhmmss_string_of_unix_second
-                              ~display_using_tz_offset_s:None x
-                            |> Result.get_ok
+                            x
+                            |> Timere.Date_time.of_timestamp
+                            |> Option.get
+                            |> Timere.Date_time.to_rfc3339
                           in
                           let y =
-                            Daypack_lib.Time.To_string
-                            .yyyymondd_hhmmss_string_of_unix_second
-                              ~display_using_tz_offset_s:None y
-                            |> Result.get_ok
+                            y
+                            |> Timere.Date_time.of_timestamp
+                            |> Option.get
+                            |> Timere.Date_time.to_rfc3339
                           in
                           Printf.sprintf "[%s, %s)" x y)
                       |> List.of_seq
