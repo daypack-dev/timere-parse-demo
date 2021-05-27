3b5b406e92062f83c1ca2119e6d049621ce83c14
Darren Ldl <darrenldldev@gmail.com>
2021-05-27 23:59:07 +1000

    Formatting

--- src/demo.ml
+++ src/demo.ml
@@ -20,11 +20,11 @@ let () =
                    resolve ~search_using_tz:tz
                      (t
                       & after
-                        (Timedesc.make_exn ~tz ~year:2000 ~month:1 ~day:1
-                           ~hour:0 ~minute:0 ~second:0 ())
+                        (Timedesc.make_exn ~tz ~year:2000 ~month:1 ~day:1 ~hour:0
+                           ~minute:0 ~second:0 ())
                       & before
-                        (Timedesc.make_exn ~tz ~year:2050 ~month:1 ~day:1
-                           ~hour:0 ~minute:0 ~second:0 ())))
+                        (Timedesc.make_exn ~tz ~year:2050 ~month:1 ~day:1 ~hour:0
+                           ~minute:0 ~second:0 ())))
                with
                | Error msg -> write_error msg
                | Ok s -> (
